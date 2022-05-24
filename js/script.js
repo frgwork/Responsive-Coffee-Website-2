87ae02805f7a0e6550ddef308d39b5f1dc5c0c9e
frgwork <frgwork@ya.ru>
2022-05-25 00:24:51 +0300

    scrollY

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ const swiper2 = new Swiper(".review-swiper", {
    grabCursor: true,
    loop: true,
    pagination: {
-      el: ".swiper-pagination",
+      el: ".tesitswiper-pagination",
       clickable: true,
    },
    navigation: {
@@ -42,15 +42,46 @@ if (navMenu && navOpen) {
 if (navMenu && navClose) {
    navClose.addEventListener('click', () => {
       navMenu.classList.remove('open');
-      body.style.overflowY = 'Scroll'
+      body.style.overflowY = 'Scroll';
    })
 }
 
 // Change header bg color
+window.addEventListener('scroll', () => {
+   const scrollY = window.pageYOffset;
+   if (scrollY > 5) {
+      document.querySelector('header').classList.add('header-active');
+   } else {
+      document.querySelector('header').classList.remove('header-active');
+   }
 
+   // Scroll up button
+   const scrollUpBtn = document.querySelector('.scrollup-btn');
+   if (scrollY > 250) {
+      scrollUpBtn.classList.add('scrollbtn-active');
+   } else {
+      scrollUpBtn.classList.remove('scrollbtn-active');
+   }
 
-// Scroll up button
+   // Nav link indicator
+   const sections = document.querySelectorAll('section[id');
 
-// Nav link indicator
+   sections.forEach(section => {
+      const sectionHeight = section.offsetHeight,
+         sectionTop = section.offsetTop - 60;
+
+      let navId = document.querySelector(`.menu-content a[href*=${section.id}]`);
+      if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+         navId.classList.add('active-navlink');
+      } else {
+         navId.classList.remove('active-navlink');
+      }
+
+      navId.addEventListener('click', () => {
+         navMenu.classList.remove('open');
+         body.style.overflowY = 'Scroll';
+      })
+   })
+})
 
 // Scroll Reveal Animation
